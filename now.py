908678196fcccb532b2a079424812d8ab031c73f
Zaev Roman <63601919+romazai@users.noreply.github.com>
2020-04-26 17:23:33 +0400

    Delete now.py

--- now.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-def update(data, string):
-    newdata = pd.read_csv(data)
-    df = pd.DataFrame({'Questions': [string], 'Answers': ['']})
-    newdata = newdata.concat([newdata, df])
-    return newdata.to_csv('question.csv')
